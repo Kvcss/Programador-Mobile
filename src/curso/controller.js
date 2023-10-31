25b985275c4d955fbcf4404a03278ea1c3335fe5
Kvcss <kaiovsilva60@gmail.com>
2023-10-30 21:55:27 -0300

    ajustando

--- src/curso/controller.js
+++ src/curso/controller.js
@@ -1,10 +1,11 @@
 const pool = require('../../db');
 const queries = require('./queries');
 
-const getCursos = (req, res)=>{queries.getCursos, (error, results)=>{
+const getCursos = (req, res)=>{pool.query(queries.getCursos, (error, results)=>{
         if(error) throw error;
         res.status(200).json(results.rows);
-    };
+
+});
 }
 
 module.exports = {
